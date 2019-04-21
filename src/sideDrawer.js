f8b6905e7606084e21027be83df8b46832ad9198
Anangcihuy06 <49478721+Anangcihuy06@users.noreply.github.com>
2019-04-21 16:25:40 +0700

    side-drawer-app
    add side drawer menu

--- src/sideDrawer.js
+++ src/sideDrawer.js
@@ -4,15 +4,16 @@ import { withStyles } from "@material-ui/core/styles";
 import SwipeableDrawer from "@material-ui/core/SwipeableDrawer";
 import Button from "@material-ui/core/Button";
 import List from "@material-ui/core/List";
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import IconButton from "@material-ui/core/IconButton";
 import Divider from "@material-ui/core/Divider";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
 import ListItemText from "@material-ui/core/ListItemText";
 import InboxIcon from "@material-ui/icons/MoveToInbox";
 import MailIcon from "@material-ui/icons/Mail";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
 
 const styles = {
@@ -24,73 +25,107 @@ const styles = {
   }
 };
 
-function SwipeableTemporaryDrawer(props) {
-  const { toggleDrawer, store } = props;
-  const { state, dispatch } = store;
-  const [drawer, setDrawer] = React.useState(false);
-  const { classes } = props;
-  const right = props;
+class SwipeableTemporaryDrawer extends React.Component {
+  state = {
+    top: false,
+    left: false,
+    bottom: false,
+    right: false
+  };
 
-  const sideList = (
-    <div className={classes.list}>
-      <List>
-        {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
-          <ListItem button key={text}>
-            <ListItemIcon>
-              {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-            </ListItemIcon>
-            <ListItemText primary={text} />
-          </ListItem>
-        ))}
-      </List>
-      <Divider />
-      <List>
-        {["All mail", "Trash", "Spam"].map((text, index) => (
-          <ListItem button key={text}>
-            <ListItemIcon>
-              {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-            </ListItemIcon>
-            <ListItemText primary={text} />
-          </ListItem>
-        ))}
-      </List>
-    </div>
-  );
-  console.log(state.App);
-  return (
-    <div>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            className={classes.menuButton}
-            color="inherit"
-            aria-label="Menu"
-          >
-            <MenuIcon onClick={() => toggleDrawer(state, dispatch)} />
-          </IconButton>
-          <Button color="inherit">The News Box office movie</Button>
-        </Toolbar>
-      </AppBar>
-      {!state.App.sideDrawer && (
+  toggleDrawer = (side, open) => () => {
+    this.setState({
+      [side]: open
+    });
+  };
+
+  render() {
+    const { classes } = this.props;
+
+    const sideList = (
+      <div className={classes.list}>
+        <List>
+          {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+        <Divider />
+        <List>
+          {["All mail", "Trash", "Spam"].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+      </div>
+    );
+
+    const fullList = (
+      <div className={classes.fullList}>
+        <List>
+          {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+        <Divider />
+        <List>
+          {["All mail", "Trash", "Spam"].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+      </div>
+    );
+
+    return (
+      <div>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              className={classes.menuButton}
+              color="inherit"
+              aria-label="Menu"
+            >
+              <MenuIcon onClick={this.toggleDrawer("right", true)} />
+            </IconButton>
+            <Button color="inherit">list movies favourites</Button>
+          </Toolbar>
+        </AppBar>
         <SwipeableDrawer
           anchor="right"
-          open={right}
-          onClose={() => toggleDrawer(state, dispatch)}
-          onOpen={() => toggleDrawer(state, dispatch)}
+          open={this.state.right}
+          onClose={this.toggleDrawer("right", false)}
+          onOpen={this.toggleDrawer("right", true)}
         >
           <div
             tabIndex={0}
             role="button"
-            onKeyDown={() => toggleDrawer(state, dispatch)}
-            onClick={() => toggleDrawer(state, dispatch)}
-            onKeyDown={() => toggleDrawer(state, dispatch)}
+            onClick={this.toggleDrawer("right", false)}
+            onKeyDown={this.toggleDrawer("right", false)}
           >
             {sideList}
           </div>
         </SwipeableDrawer>
-      )}
-    </div>
-  );
+      </div>
+    );
+  }
 }
 
 SwipeableTemporaryDrawer.propTypes = {
